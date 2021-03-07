688ea20c52b2e87ec2c1272ebfa5fd08b7af7498
JordanLevy99 <jdlevy@ucsd.edu>
2021-03-06 19:27:38 -0800

    modified run.py to generate counterfactuals

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 import json
 import sys
-import torch
-from src.utils import create_input_files
-from src.train import main
-from src.eval import evaluate
-from src.caption import caption_image_beam_search, visualize_att
+# import torch
+# from src.utils import create_input_files
+# from src.train import main
+# from src.eval import evaluate
+# from src.caption import caption_image_beam_search, visualize_att
+from src.counterfactuals import create_mask_input, generate_counterfactuals
 
 def data(test=False):
     root = ''
@@ -12,7 +13,7 @@ def data(test=False):
         f = open(root+"config/create_input_files_test.json")
     else:
         f = open(root+"config/create_input_files.json")
-    jsonread = json.load(f) 
+    jsonread = json.load(f)
     create_input_files(dataset=jsonread['dataset'],
                        karpathy_json_path=root+jsonread['karpathy_json_path'],
                        image_folder=jsonread['image_folder'],
@@ -20,10 +21,10 @@ def data(test=False):
                        min_word_freq=jsonread['min_word_freq'],
                        output_folder=root+jsonread['output_folder'],
                        max_len=jsonread['max_len'],)
-    
+
 def train(test=False):
     main(test)
-    
+
 def evaluate_model(test=False):
     if test:
         f = open('config/eval_test.json')
@@ -32,15 +33,16 @@ def evaluate_model(test=False):
     jsonread = json.load(f)
     beam_size = jsonread['beam_size']
     print("\nBLEU-4 score @ beam size of %d is %.4f." % (beam_size, evaluate(beam_size, test)))
-    
+
+
 def generate_viz(test=False):
     if test:
         f = open('config/caption_test.json')
     else:
         f = open('config/caption.json')
-    jsonread = json.load(f) 
+    jsonread = json.load(f)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
+
     # Load model
     checkpoint = torch.load(jsonread['model_fp'], map_location=str(device))
     decoder = checkpoint['decoder']
@@ -61,24 +63,48 @@ def generate_viz(test=False):
 
     # Visualize caption and attention of best sequence
     visualize_att(jsonread['img_fp'], seq, alphas, rev_word_map, jsonread['smooth'])
-    
+
+def counterfactual_production(test=False, year=None):
+    if test:
+        fname = 'config/counterfactual_test.json'
+    else:
+        fname = 'config/counterfactual.json'
+    if year == '2014':
+        fname = 'config/counterfactual_2014.json'
+
+    with open(fname, 'r') as f:
+        jsonread = json.load(f)
+    data_dir = jsonread['data_dir']
+    temp_dir = jsonread['temp_dir']
+    out_dir = jsonread['out_dir']
+    annotation_fp = jsonread['annotation_fp']
+    checkpoint_dir = jsonread['checkpoint_dir']
+    model_id = jsonread['model_id']
+
+    create_mask_input(data_dir, temp_dir, out_dir, annotation_fp, False)
+    generate_counterfactuals(temp_dir, checkpoint_dir, model_id)
+
 def all():
     data()
     train()
     evaluate_model()
     generate_viz()
-    
+
+def coco2014():
+    counterfactual_production(year='2014')
+
 def test():
-    data(True)
-    train(True)
-    evaluate_model(True)
-    generate_viz(True)
+    # data(True)
+    # train(True)
+    # evaluate_model(True)
+    counterfactual_production(test=True)
+    # generate_viz(True)
 
 if __name__ == '__main__':
-    try:
-        globals()[sys.argv[1]]()
-    except:
-        data()
-        train()
-        evaluate_model()
-        generate_viz()
+    # try:
+    globals()[sys.argv[1]]()
+    # except IndexError:
+    #     data()
+    #     train()
+    #     evaluate_model()
+    #     generate_viz()
